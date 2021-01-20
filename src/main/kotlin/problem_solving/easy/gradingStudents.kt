7d1cbe6c7fe9656a9e5063c5a41328caffdb1d10
qu4dro <33917440+qu4dro@users.noreply.github.com>
2021-01-20 19:03:39 +0800

    Grading students problem completed

--- src/main/kotlin/problem_solving/easy/gradingStudents.kt
+++ src/main/kotlin/problem_solving/easy/gradingStudents.kt
@@ -1,4 +1,54 @@
 package problem_solving.easy
 
-class gradingStudents {
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+fun gradingStudents(grades: Array<Int>): Array<Int> {
+    for (i in grades.indices) {
+        val tmp = grades[i] % 10
+        if (grades[i] > 37) {
+            when {
+                tmp in 3..4 -> grades[i] += 5 - tmp
+                tmp > 7 -> grades[i] += 10 - tmp
+            }
+        }
+    }
+    return grades
+}
+
+fun main(args: Array<String>) {
+    val gradesCount = readLine()!!.trim().toInt()
+
+    val grades = Array<Int>(gradesCount) { 0 }
+    for (i in 0 until gradesCount) {
+        val gradesItem = readLine()!!.trim().toInt()
+        grades[i] = gradesItem
+    }
+
+    val result = gradingStudents(grades)
+
+    println(result.joinToString("\n"))
 }
